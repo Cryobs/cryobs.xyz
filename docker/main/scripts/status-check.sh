7d43a5ec69ab21316cb17c96e78eefd66da00b35
Cryobs <cryobs.ads@gmail.com>
2025-11-01 18:17:36 +0100

    fix status-check

--- docker/main/scripts/status-check.sh
+++ docker/main/scripts/status-check.sh
@@ -11,12 +11,13 @@ mapfile -t NAMES < <(mariadb -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" -D "$DB_NA
   "SELECT name FROM sys_status;")
 
 for i in ${!URLS[@]}; do 
-  status=$(curl -Is ${URLS[$i]} | head -1 | cut -d " " -f 2);
-  if  [[ "$status" = "200" ]]; then
+  status_code=$(curl -isL ${URLS[$i]} | head -1 | cut -d " " -f 2);
+  if  [[ "$status_code" = "200" ]]; then
     status="OK"
   else
     status="ERROR"
   fi
+  echo "${URLS[$i]}; ${NAMES[$i]}; $status_code"
   mariadb -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -Bse \
     "REPLACE INTO sys_status (url, name, status) VALUES ('${URLS[$i]}', '${NAMES[$i]}', '$status');"
 done
